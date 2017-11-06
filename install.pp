8dccddd6eb19f5ad860a89ed68ec8656e2d0200e
root <root@jerrycarter1.mylabserver.com>
2017-11-06 08:01:46 +0000

    fix some bugs

--- install.pp
+++ install.pp
@@ -1,5 +1,5 @@
-class ntp::install(
-  Srting $package_name   = $ntp::package_name,
+class ntp::install (
+  String $package_name   = $ntp::package_name,
   String $package_ensure = $ntp::package_ensure,
 ) {
   package { $package_name:
